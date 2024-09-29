f6787a65343de548b38ad4175a1a01fa0ac5d4dc
João Vitor Sousa Ramos <79048652+Kaneliodas@users.noreply.github.com>
2024-09-29 14:33:31 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+a = list(range(1,11))
+#Criando uma lista de números 
+squares = []
+#Deixando uma lista para armazenar futuramente 
+for value in a: squares.append(value**2)
+#Armazenando a lista a em value para em seguida armazenarna lista squares
+print(squares) 
+#Mostrar a lista squares
